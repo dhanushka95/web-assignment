7c8fd3e1f24cba25acdb9c41ea133008c9618282
dhanushka95 <danushkadayawansha44@gmail.com>
2020-08-01 01:35:19 +0530

    fix port err

--- app.js
+++ app.js
@@ -17,4 +17,7 @@ app.use('/information',require('./routes/informationR'));
 app.get("/", (req, res) =>
     res.send("Welcome!")
 );
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log("Server listening on port:", PORT);
+});
